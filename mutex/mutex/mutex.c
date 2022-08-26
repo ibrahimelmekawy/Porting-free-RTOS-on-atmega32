76119afce55ffd5ea2b96b8c8b444a7892eafc20
Ibrahim Elmekawy <87153297+ibrahimelmekawy@users.noreply.github.com>
2022-08-26 17:10:06 +0200

    Add files via upload

---
+++ mutex/mutex/mutex.c
@@ -0,0 +1,49 @@
+#include <avr/io.h>
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include <avr/interrupt.h>
+#include "semphr.h"
+int x,y;
+void task1(void *pv);
+void task2 (void *pv);
+SemaphoreHandle_t my_mutex;
+int main(void)
+{
+	xTaskCreate(task1,"M",100,NULL,2,NULL);
+	xTaskCreate(task2,"N",100,NULL,1,NULL);
+	my_mutex=xSemaphoreCreateMutex();
+	vTaskStartScheduler();
+    while(1)
+    {
+        
+    }
+}
+void task1(void *pv)
+{
+	DDRD|=(1<<7);
+	while(1)
+	{
+		xSemaphoreTake(my_mutex,20);
+		if(x!=y)
+		{
+			PORTD^=(1<<7);
+		}
+		xSemaphoreGive(my_mutex);
+		vTaskDelay(100);
+	}
+	
+}	
+void task2 (void *pv)
+{
+	while(1)
+	{
+		xSemaphoreTake(my_mutex,1000);
+		++x;
+		_delay_ms(200);
+		++y;
+	//	xSemaphoreGive(my_mutex);
+	}
+}
+
